da609bf96a5fac3c733a82d83f0f6a08a3c0737e
Javier Tiá <javier.tia@gmail.com>
2016-04-01 03:36:51 -0600

    Format exercise 26 code

--- ex26/ex26.py
+++ ex26/ex26.py
@@ -1,34 +1,41 @@
 from ex25 import *
 
+
 def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
     return words
 
+
 def sort_words(words):
     """Sorts the words."""
     return sorted(words)
+
 
 def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
     print word
 
+
 def print_last_word(words):
     """Prints the last word after popping it off."""
     word = words.pop(-1)
     print word
+
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
     return sort_words(words)
 
+
 def print_first_and_last(sentence):
     """Prints the first and last words of the sentence."""
     words = break_words(sentence)
     print_first_word(words)
     print_last_word(words)
+
 
 def print_first_and_last_sorted(sentence):
     """Sorts the words then prints the first and last one."""
@@ -49,7 +56,6 @@ and requires an explanation
 \n\t\twhere there is none.
 """
 
-
 print "--------------"
 print poem
 print "--------------"
@@ -57,11 +63,13 @@ print "--------------"
 five = 10 - 2 + 3 - 6
 print "This should be five: %s" % five
 
+
 def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
+
 
 start_point = 10000
 beans, jars, crates = secret_formula(start_point)
@@ -73,7 +81,6 @@ start_point = start_point / 10
 
 print "We can also do that this way:"
 print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
-
 
 sentence = "All god\tthings come to those who weight."
 
@@ -90,4 +97,3 @@ print sorted_words
 print_first_and_last(sentence)
 
 print_first_and_last_sorted(sentence)
-
